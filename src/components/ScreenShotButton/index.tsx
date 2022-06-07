027f444a40f7385b45ad6130e93617d72bbdbcf8
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-06-07 10:27:10 -0300

    feat: Created component ScreenShotButton

---
+++ src/components/ScreenShotButton/index.tsx
@@ -0,0 +1,41 @@
+import { Camera, Trash } from "phosphor-react-native";
+import React from "react";
+import { View, TouchableOpacity, Image } from "react-native";
+import { theme } from "../../theme";
+
+import { styles } from "./styles";
+
+interface Props {
+  screenshot: string | null;
+  onTakeShot: () => void;
+  onRemoveShot: () => void;
+}
+
+const ScreenShotButton: React.FC<Props> = ({
+  screenshot,
+  onTakeShot,
+  onRemoveShot,
+}) => {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={screenshot ? onRemoveShot : onTakeShot}
+    >
+      {screenshot ? (
+        <View>
+          <Image source={{ uri: screenshot }} style={styles.image} />
+          <Trash
+            size={24}
+            weight="fill"
+            color={theme.colors.text_secondary}
+            style={styles.removeIcon}
+          />
+        </View>
+      ) : (
+        <Camera size={24} weight="fill" color={theme.colors.text_secondary} />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default ScreenShotButton;

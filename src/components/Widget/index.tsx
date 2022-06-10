73fd13e31ef2ca87b91cc835b2a0ae72ca6a06a4
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-06-10 11:49:01 -0300

    feat: Implements components in Widget

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { TouchableOpacity } from "react-native";
 import { ChatTeardropDots } from "phosphor-react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
@@ -6,13 +6,31 @@ import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
 import { theme } from "../../theme";
 import { styles } from "./styles";
+
 import Options from "../Options";
+import Form from "../Form";
+import Success from "../Success";
+
+import { feedbackTypes } from "../../utils/feedbackTypes";
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 const Widget: React.FC = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   const handleOpen = () => {
     bottomSheetRef.current?.expand();
+  };
+
+  const handleRestartFeedback = () => {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  };
+
+  const handleFeedbackSent = () => {
+    setFeedbackSent(true);
   };
 
   return (
@@ -30,7 +48,21 @@ const Widget: React.FC = () => {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        {feedbackSent ? (
+          <Success />
+        ) : (
+          <>
+            {feedbackType ? (
+              <Form
+                feedbackType={feedbackType}
+                onFeedbackCanceled={handleRestartFeedback}
+                onFeedbackSent={handleFeedbackSent}
+              />
+            ) : (
+              <Options onFeedbackTypeChanged={setFeedbackType} />
+            )}
+          </>
+        )}
       </BottomSheet>
     </>
   );

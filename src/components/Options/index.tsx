85f49241c3aeb93c0f9d47875907cb7f0d7d5bf4
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-06-10 11:48:23 -0300

    refactor: Refactor component Options

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -4,16 +4,26 @@ import { feedbackTypes } from "../../utils/feedbackTypes";
 
 import Copyright from "../Copyright";
 import Option from "../Option";
+import { FeedbackType } from "../Widget";
 
 import { styles } from "./styles";
 
-const Options: React.FC = () => {
+interface Props {
+  onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
+
+const Options: React.FC<Props> = ({ onFeedbackTypeChanged }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Deixe seu Feedback</Text>
       <View style={styles.options}>
         {Object.entries(feedbackTypes).map(([key, value]) => (
-          <Option key={key} title={value.title} image={value.image} />
+          <Option
+            key={key}
+            title={value.title}
+            image={value.image}
+            onPress={() => onFeedbackTypeChanged(key as FeedbackType)}
+          />
         ))}
       </View>
       <Copyright />
